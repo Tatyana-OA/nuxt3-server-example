fec9ce2749009b2449f51f88e02ac120a60c088b
TAsenova <tasenova@mentormate.com>
2024-10-30 16:28:08 +0200

    Fetching API data with useFetch composable

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
+<script setup>
+const { data: scizor } = await useFetch(
+  "https://pokeapi.co/api/v2/pokemon/scizor"
+);
+</script>
+
 <template>
-  <h1>Homepage</h1>
+  <ul>
+    <li><img :src="scizor.sprites.front_default" alt="pokeImage" /></li>
+    <li>Pokemon ID: {{ scizor.id }}</li>
+    <li>Pokemon Name: {{ scizor.name }}</li>
+    <li>Pokemon Types: {{ scizor.types }}</li>
+  </ul>
+  <div>
+    <pre>
+      {{ scizor }}
+    </pre>
+  </div>
 </template>
