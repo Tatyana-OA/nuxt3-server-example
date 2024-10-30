cf57fc056ac50a9fe26116262e263388b6ac35c6
TAsenova <tasenova@mentormate.com>
2024-10-30 16:53:10 +0200

    Error Handling

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <script setup>
-const { data: scizor } = await useFetch("/api/scizor");
+const { data: pokemon, error } = await useFetch("/api/pokemon");
 </script>
 <template>
-  <ul>
-    <li><img :src="scizor.sprite" alt="sprite" /></li>
-    <li>Pokemon ID: {{ scizor.id }}</li>
-    <li>Pokemon Name: {{ scizor.name }}</li>
+  <ul v-if="pokemon">
+    <li><img :src="pokemon.sprite" alt="sprite" /></li>
+    <li>Pokemon ID: {{ pokemon.id }}</li>
+    <li>Pokemon Name: {{ pokemon.name }}</li>
   </ul>
+  <div v-else>
+    {{ error }}
+  </div>
 </template>
--- server/api/[pokemon].js
+++ server/api/[pokemon].js
@@ -2,13 +2,22 @@ export default defineEventHandler(async (event) => {
   // $fetch - server - specific
 
   const { pokemon } = event.context.params;
-  const response = await $fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  try {
+    const response = await $fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    );
 
-  const idealResponse = {
-    id: response.id,
-    name: response.name,
-    sprite: response.sprites.front_default,
-  };
+    const idealResponse = {
+      id: response.id,
+      name: response.name,
+      sprite: response.sprites.front_default,
+    };
 
-  return idealResponse;
+    return idealResponse;
+  } catch (err) {
+    throw createError({
+      statusCode: 404,
+      message: "Can't find that Pokemon! Sowy!",
+    });
+  }
 });
